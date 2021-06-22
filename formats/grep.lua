051611171195d427bc88817677583700ec59c0d4
PhilipRoman <dev.philipjohn@gmail.com>
2021-06-22 16:11:50 +0300

    Add grep format

---
+++ formats/grep.lua
@@ -0,0 +1,47 @@
+#!/usr/bin/env lua5.3
+
+return {
+	sl = 'sl',
+	['selected lines'] = 'sl',
+	['selected line'] = 'sl',
+
+	cx = 'cx',
+	['context lines'] = 'cx',
+	['context line'] = 'cx',
+
+	mt = 'mt',
+	matching = 'mt',
+
+	ms = 'ms',
+	['matching selected'] = 'ms',
+
+	mc = 'mc',
+	['matching context'] = 'mc',
+	['matching contexts'] = 'mc',
+
+	fn = 'fn',
+	file = 'fn',
+	files = 'fn',
+	name = 'fn',
+	names = 'fn',
+	filename = 'fn',
+	filenames = 'fn',
+	['file name'] = 'fn',
+	['file names'] = 'fn',
+
+	ln = 'ln',
+	number = 'ln',
+	numbers = 'ln',
+	['line number'] = 'ln',
+	['line numbers'] = 'ln',
+
+	bn = 'bn',
+	byte = 'bn',
+	bytes = 'bn',
+	['byte offset'] = 'bn',
+	['byte offsets'] = 'bn',
+
+	se = 'se',
+	separator = 'se',
+	separators = 'se',
+}
